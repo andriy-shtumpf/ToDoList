e0117a18a30b73153b4e31ab79d0a5cd7b5f84ea
andriyscode <shtumpf@gmail.com>
2021-03-29 21:51:37 -0400

    Initial commit

---
+++ TEXT_CLASSES_CHANGES/Controller.java
@@ -0,0 +1,99 @@
+package com.shtumpf.todolist;
+
+import com.shtumpf.todolist.datamodule.ToDoData;
+import com.shtumpf.todolist.datamodule.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class Controller {
+    private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    @FXML
+    private Label deadlineLabel;
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+    public void initialize(){
+//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+//                LocalDate.of(2016, Month.APRIL, 25));
+//
+//        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+//                LocalDate.of(2016, Month.MAY, 23));
+//
+//        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website" +
+//                " mockups by Friday 22nd April", LocalDate.of(2016, Month.APRIL, 22));
+//
+//        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
+//                LocalDate.of(2016, Month.MARCH, 23));
+//
+//        TodoItem item5 = new TodoItem("Pickup dry cleaning", "The clothes should be ready by Wednesday",
+//                LocalDate.of(2016, Month.APRIL, 20));
+//
+//        todoItems = new ArrayList<>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
+                if(newValue != null){
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));       // with data format
+//                    deadlineLabel.setText(item.getDeadline().toString());
+                }
+            }
+        });
+
+//        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(ToDoData.getInstance().getTodoItems());
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void showNewItemDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+        }catch (IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module ToDoList {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+//    requires java.sql;   in java11
+
+    requires graficx;
+
+    opens com.shtumpf.todolist;
+}
